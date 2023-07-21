68422ba59f0c768511a18a34b158adadf4a14428
jnaoroji <jennynaoroji@gmail.com>
2023-07-21 17:19:26 +1000

    seed data updated

--- utils/data.js
+++ utils/data.js
@@ -74,27 +74,39 @@ const names = [
   'Parker',
 ];
 
-const appDescriptions = [
-  'Decision Tracker',
-  'Find My Phone',
-  'Learn Piano',
-  'Starbase Defender',
-  'Tower Defense',
-  'Monopoly Money Manager',
-  'Movie trailers',
-  'Hello world',
-  'Stupid Social Media App',
-  'Notes',
-  'Messages',
-  'Email',
-  'Compass',
-  'Firefox',
-  'Running app',
-  'Cooking app',
-  'Poker',
-  'Deliveries',
-];
+// const appDescriptions = [
+//   'Decision Tracker',
+//   'Find My Phone',
+//   'Learn Piano',
+//   'Starbase Defender',
+//   'Tower Defense',
+//   'Monopoly Money Manager',
+//   'Movie trailers',
+//   'Hello world',
+//   'Stupid Social Media App',
+//   'Notes',
+//   'Messages',
+//   'Email',
+//   'Compass',
+//   'Firefox',
+//   'Running app',
+//   'Cooking app',
+//   'Poker',
+//   'Deliveries',
+// ];
 
+const thoughtDescriptions = [
+  'I wonder what life would be like if we could communicate with animals and understand their perspectives.',   
+  'The smell of freshly baked cookies always brings back nostalgic memories.', 
+  'Time seems to pass differently when youre fully engaged in something you love.',
+  'The night sky is a mesmerizing canvas of stars, each one potentially hosting its own set of planets and life.', 
+  'I wish more people would practice empathy and kindness towards one another, the world could be a better place.', 
+  'Its fascinating how technology has evolved, and I wonder what groundbreaking innovations await us in the future.', 
+  'I should really pick up that book Ive been wanting to read for ages and dive into its world.', 
+  'Memories are like pieces of a puzzle, sometimes fitting perfectly, and other times leaving gaps we cant seem to fill.', 
+  'Traveling to different countries allows you to experience diverse cultures, broadening your understanding of the world.', 
+  'I find solace in nature, and taking a walk in the woods always brings a sense of peace and clarity to my mind.', 
+];
 // Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
@@ -103,16 +115,25 @@ const getRandomName = () =>
   `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
 
 // Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
+// const getRandomAssignments = (int) => {
+//   const results = [];
+//   for (let i = 0; i < int; i++) {
+//     results.push({
+//       assignmentName: getRandomArrItem(appDescriptions),
+//       score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+//     });
+//   }
+//   return results;
+// };
+// Function to generate random thoughts that we can add to user object.
+const getRandomThoughts = (int) => {
   const results = [];
   for (let i = 0; i < int; i++) {
     results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+      thoughtText: getRandomArrItem(thoughtDescriptions),
     });
   }
   return results;
 };
-
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+module.exports = { getRandomName, getRandomThoughts };
