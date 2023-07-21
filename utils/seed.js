1301fb8f3321b8b7a4c97473e7543ae77c702c4b
jnaoroji <jennynaoroji@gmail.com>
2023-07-21 20:00:50 +1000

    thoughts functional in GET users route

--- utils/seed.js
+++ utils/seed.js
@@ -20,31 +20,46 @@ connection.once('open', async () => {
 
   // Create empty array to hold the users
   const users = [];
+
   
 
   // Loop 20 times -- add users to the users array
   for (let i = 0; i < 20; i++) {
+    
     // Get some random thought objects using a helper function that we imported from ./data
     const thoughts = getRandomThoughts(5);
-    console.log(thoughts);
+    //insert into thoughts collection
+    await Thought.collection.insertMany(thoughts);
+    // get thought ids from array
+    const allThoughtIds = thoughts.map((thought) => thought._id);
+      
+
+    
     const fullName = getRandomName();
     const first = fullName.split(' ')[0];
-    // const last = fullName.split(' ')[1];
     const username = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
     const email = `${username}@email.com`;
 
-    users.push({
-      username, 
-      email, 
-      thoughts
-    });
-    
+    const user = {
+      username,
+      email,
+      thoughts: allThoughtIds, 
+    };
+    // Add friends to the user
+    if (i > 0) {
+      // Assuming you want to add at least one friend to each user
+      const randomFriendIndex = Math.floor(Math.random() * i); // Select a random user as a friend
+      const randomFriend = users[randomFriendIndex];
+      user.friends = [randomFriend._id]; // Add the friend's _id to the user's friends array
+    }
+    users.push(user);
+
   }
 
   // Add users to the collection and await the results
   await User.collection.insertMany(users);
 
-  // // Add thoughts to the collection and await the results
+  // Add thoughts to the collection and await the results
   // await Thought.collection.insertOne({
   //   thoughtText: 'This is my thought',
   //   users: [...users],
@@ -52,7 +67,7 @@ connection.once('open', async () => {
 
   // Log out the seed data to indicate what should appear in the database
   console.table(users);
-  console.table(users.thoughts);
+  
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
