2863a387bd295e5f72b8539963e92327d64ff04f
jnaoroji <jennynaoroji@gmail.com>
2023-07-22 20:04:44 +1000

    reactions POST and DELETE routes working, Thoughts GET, GET by id and POST route working, friends POST and DELETE route working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -32,7 +32,7 @@ module.exports = {
       const thought = await Thought.create(req.body);
       console.log(req.body);
       const userId = (req.params.userId);
-      await User.findOneAndUpdate(userId, { $push: { thoughts: thought._id } });
+      await User.findOneAndUpdate({_id: userId}, { $push: { thoughts: thought._id } });
       res.json(thought);
       console.log(thought);
       console.log(User);
@@ -45,38 +45,38 @@ module.exports = {
   },
 
   // Delete a thought
-  async deleteThought(req, res) {
-    try {
-      const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+  // async deleteThought(req, res) {
+  //   try {
+  //     const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
 
-      if (!thought) {
-        res.status(404).json({ message: 'No thought with that ID' });
-      }
+  //     if (!thought) {
+  //       res.status(404).json({ message: 'No thought with that ID' });
+  //     }
 
-      await Thought.deleteMany({ _id: { $in: User.thoughts } });
-      res.json({ message: 'user and thoughts deleted!' });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+  //     await Thought.deleteMany({ _id: { $in: User.thoughts } });
+  //     res.json({ message: 'user and thoughts deleted!' });
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // },
   // Update a thought
-  async updateThought(req, res) {
-    try {
-      const thought = await Thought.findOneAndUpdate(
-        { _id: req.params.thoughtId },
-        { $set: req.body },
-        { runValidators: true, new: true }
-      );
+  // async updateThought(req, res) {
+  //   try {
+  //     const thought = await Thought.findOneAndUpdate(
+  //       { _id: req.params.thoughtId },
+  //       { $set: req.body },
+  //       { runValidators: true, new: true }
+  //     );
 
-      if (!thought) {
-        res.status(404).json({ message: 'No thought with this id!' });
-      }
+  //     if (!thought) {
+  //       res.status(404).json({ message: 'No thought with this id!' });
+  //     }
 
-      res.json(thought);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+  //     res.json(thought);
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // },
   //create a reaction to be stored in a single thoughts reactions array
   async addReaction(req, res) {
     console.log('You are adding a reaction to this thought');
@@ -84,7 +84,7 @@ module.exports = {
     try {
       const thought = await Thought.findOneAndUpdate(
         { _id: req.params.thoughtId },
-        { $addToSet: { reaction: req.body } },
+        { $addToSet: { reactions: req.body } },
         { runValidators: true, new: true }
       );
       if (!thought) {
@@ -99,10 +99,12 @@ module.exports = {
   },
     // Remove a reaction by the reactions id field
     async removeReaction(req, res) {
+      console.log('You are removing a reaction on this thought');
       try {
-        const thought = await thought.findOneAndUpdate(
-          { _id: req.params.thoguthId },
-          { $pull: { reaction: { reactionId: req.params.reactionId } } },
+        const thought = await Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $pull: { reactions: { reactionId: req.params.reactionId } } },
+          // { $pull: { reactions: req.params.reactionId } },
           { runValidators: true, new: true }
         );
   
@@ -117,4 +119,23 @@ module.exports = {
         res.status(500).json(err);
       }
     },
+    // async removeFriend(req, res) {
+    //   try {
+    //     const user = await User.findOneAndUpdate(
+    //       { _id: req.params.userId },
+    //       { $pull: { friends: req.params.friendId  } },
+    //       { runValidators: true, new: true }
+    //     );
+  
+    //     if (!user) {
+    //       return res
+    //         .status(404)
+    //         .json({ message: 'No user found with that ID :(' });
+    //     }
+  
+    //     res.json(user);
+    //   } catch (err) {
+    //     res.status(500).json(err);
+    //   }
+    // }, 
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,8 +3,8 @@ const {
   getThoughts,
   getSingleThought,
   createThought,
-  updateThought,
-  deleteThought,
+  // updateThought,
+  // deleteThought,
   addReaction, 
   removeReaction,
 } = require('../../controllers/thoughtController.js');
@@ -16,13 +16,14 @@ router.route('/').get(getThoughts).post(createThought);
 router
   .route('/:thoughtId')
   .get(getSingleThought)
-  .put(updateThought)
-  .delete(deleteThought);
+  // .put(updateThought)
+  // .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions').post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
 
 module.exports = router;
